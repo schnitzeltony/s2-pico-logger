2e2055b2ef2c732b146f8d5560941be8c4314ff6
Andreas Müller <schnitzeltony@gmail.com>
2024-09-10 20:43:14 +0200

    Introduce echo for test
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/main.c
+++ src/main.c
@@ -8,6 +8,17 @@ int main() {
     led_switch(true);
 
     while (1) {
-        tight_loop_contents();
+        queue_t *rx_queue = getRxQueue(0);
+        queue_t *tx_queue = getTxQueue(0);
+        bool starRequired = false;
+        while(!queue_is_empty(rx_queue)) {
+            starRequired = true;
+            uint8_t character;
+            queue_try_remove(rx_queue, &character);
+            queue_try_add(tx_queue, &character);
+        }
+        if(starRequired)
+            startTransmitAfterAddingTransmitData(0);
+        //tight_loop_contents();
     }
 }
