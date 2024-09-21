4846b7c16c9176c01131c61428bb02dcb259c296
Andreas Müller <schnitzeltony@gmail.com>
2024-09-21 14:45:15 +0200

    Double Ouch!!!
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/logtimestampgenerator.cpp
+++ src/logtimestampgenerator.cpp
@@ -1,7 +1,7 @@
 #include "logtimestampgenerator.h"
 #include <stdio.h>
 
-static char timeStrSeconds[sizeof("1970-01-01 00:00:0")+1];
+static char timeStrSeconds[sizeof("1970-01-01 00:00:00")+1];
 static char timeStrMs[sizeof("000")+1];
 static char timeStrTotal[sizeof(timeStrSeconds)+ sizeof(timeStrMs)];
 
@@ -9,7 +9,7 @@ const char *LogTimeStampGenerator::getTimeStampStr(std::chrono::system_clock::ti
 {
     time_t timeT = std::chrono::system_clock::to_time_t(time);
     const struct tm *lTime = localtime(&timeT);
-    strftime(timeStrSeconds, 256, getTimeStampBaseFormat(), lTime);
+    strftime(timeStrSeconds, sizeof(timeStrSeconds), getTimeStampBaseFormat(), lTime);
     const auto nowMs = std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()) % 1000;
     sprintf(timeStrMs, "%03d", nowMs.count());
     sprintf(timeStrTotal, "%s.%s", timeStrSeconds, timeStrMs);
