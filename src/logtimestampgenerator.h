4cdd2353641e895f26ac272df29c77216badceb8
Andreas Müller <schnitzeltony@gmail.com>
2024-09-21 14:58:58 +0200

    LogTimeStampGenerator: Remove unneeded struct
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/logtimestampgenerator.h
+++ src/logtimestampgenerator.h
@@ -8,7 +8,7 @@ public:
     static const char* getTimeStampStr(const std::chrono::system_clock::time_point &time);
 private:
     static const char* getTimeStampBaseFormat();
-    static const struct tm* getLocalImeStruct(const std::chrono::system_clock::time_point &time);
+    static const tm* getLocalImeStruct(const std::chrono::system_clock::time_point &time);
 };
 
 #endif // LOGTIMESTAMPGENERATOR_H
